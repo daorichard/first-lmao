535cb094c0dc73c10094aedbed26116c611a743a
daorichard <98173508+daorichard@users.noreply.github.com>
2023-06-01 00:35:31 -0400

    minor styling changes

---
+++ script.js
@@ -0,0 +1,10 @@
+const dontButton = document.getElementById("btndont");
+dontButton.addEventListener("click", pleaseNo);
+
+function pleaseNo() {
+  // iterate through alerts that keep saying messages
+
+  alert("please no");
+}
+
+// event
