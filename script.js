d0e4471512e3db3a631303b524f27e3346ec54a7
daorichard <98173508+daorichard@users.noreply.github.com>
2023-06-05 11:37:38 -0400

    more styling, added bio containers

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
-"use strict";
+'use strict';
 
 // DOM SELECTIONS
 // Buttons
-const buttonDont = document.querySelector(".btn-dont");
-const buttonTwo = document.querySelector(".btn-2");
-// Time
-let time = document.getElementById("current-time");
-
-setInterval(() => {
-  let d = new Date();
-  time.innerHTML = d.toLocaleTimeString([], {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-}, 1000);
-
-console.log(time);
+const buttonDont = document.querySelector('.btn-dont');
+const buttonTwo = document.querySelector('.btn-2');
 
 // const buttonTwo = document.getElementsByClassName("btn-2");
 function alertBtn() {
-  alert("Idk");
+  alert('Idk');
 }
 
 // EVENT LISTENERS - BUTTONS
-buttonDont.addEventListener("click", pleaseNo());
-buttonTwo.addEventListener("click", alertBtn);
+buttonDont.addEventListener('click', pleaseNo());
+buttonTwo.addEventListener('click', alertBtn);
 
 // const dontButton = document.getElementById("btndont");
 // console.log(dontButton);
@@ -32,7 +20,7 @@ buttonTwo.addEventListener("click", alertBtn);
 
 function pleaseNo() {
   // iterate through alerts that keep saying messages
-  const statements = ["please no", "dont do it", "..."];
+  const statements = ['please no', 'dont do it', '...'];
   let counter = 0;
   return () => {
     alert(statements[counter]);
